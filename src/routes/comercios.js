aab3fe3cb29149e1cab550350b7d9ea2515ebd8f
Guillermo <guillejuma@outlook.es>
2020-11-22 23:45:59 -0600

    listo la ruta a negocios

--- src/routes/comercios.js
+++ src/routes/comercios.js
@@ -1,16 +1,19 @@
+//Ruta Para adceder a los datos de los comercios
+
 const {Router} = require('express');
 const router = Router();
 const _ = require('underscore');
 const funciones = require('./funciones.js');
 const fetch = require('node-fetch');
+const L = 'leaflet'         
 
 router.post('/ubicaciones',async (req,res) => {
-    const {Estado,Municipio,Colonia,Tipo} = req.body;
+    const {Estado,Municipio,Colonia,Tipo,Ubicacion} = req.body;
     if(Estado&&Municipio&&Colonia&&Tipo){
-        console.log(funciones.region(Estado,Municipio,Colonia,Tipo));
         var region = funciones.region(Estado,Municipio,Colonia,Tipo);
         const arrCoor =[];
         const response = await fetch(region);
+        if(response.ok){
         const comercios = await response.json();
 
         _.each(comercios,(comercio,i) =>{
@@ -19,12 +22,18 @@ router.post('/ubicaciones',async (req,res) => {
                 "Latitud": "",
                 "Longitud": ""
             }
-            coor.Negocio = comercio.Nombre;
-            coor.Latitud = comercio.Latitud;
-            coor.Longitud = comercio.Longitud;
-            arrCoor.push(coor);
+            if(comercio.Ubicacion== Ubicacion){
+                coor.Negocio = comercio.Nombre;
+                coor.Latitud = comercio.Latitud;
+                coor.Longitud = comercio.Longitud;
+                arrCoor.push(coor);
+            }
         });
         res.json(arrCoor);
+        }
+        else{
+            res.status(500).json({error: 'Datos no validos'});
+        }
     }
     else{
         res.status(500).json({error: 'There was an error'});
